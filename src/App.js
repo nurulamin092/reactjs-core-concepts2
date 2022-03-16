eeecb620255e2cf58af8e9f0a526764a386ac721
nurulamin092 <mnurulamin092@gmial.com>
2022-03-16 16:10:11 +0600

    daynaic user add

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
 
   return (
     <div className="App">
-      <Counter></Counter>
+      <ExtranulUser></ExtranulUser>
     </div>
   );
 }
+function ExtranulUser() {
+  const [users, setUsers] = useState('');
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json()).
+      then(data => setUsers(data))
+  }, []);
+  return (
+    <div>
+      <h1>Extranal User</h1>
+      {
+        users.map(user => <User name={user.name} email={user.email}></User>)
+      }
 
-function Counter() {
+    </div >
+  )
+}
+function User(props) {
+  return (
+    <div className='user'>
+      <h3>Name:{props.name}</h3>
+      <p>Email:{props.email}</p>
+    </div>
+  )
+}
+/* function Counter() {
   const [count, setCount] = useState(40);
   const increaseCount = () => setCount(count + 1);
   const descreaseCount = () => setCount(count - 1);
@@ -24,7 +48,7 @@ function Counter() {
       <button onClick={descreaseCount}>Descrease</button>
     </div>
   )
-}
+} */
 
 
 export default App;
