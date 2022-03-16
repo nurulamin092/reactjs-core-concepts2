d6ef6a91086e37aac88bbf8f4e82f149c13e2ed8
nurulamin092 <mnurulamin092@gmial.com>
2022-03-16 12:20:45 +0600

    daynamic component

--- src/App.js
+++ src/App.js
@@ -2,24 +2,29 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const products = [
+    { name: 'laptop', price: 900000 },
+    { name: 'phone', price: 55000 },
+    { name: 'watch', price: 9000 },
+    { name: 'tablet', price: 12000 },
+
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        products.map(product => <Product name={product.name} price={product.price}></Product>)
+      }
     </div>
   );
+}
+
+function Product(props) {
+  return (
+    <div className="product">
+      <h2>Name:{props.name}</h2>
+      <h3>Price:{props.price}</h3>
+    </div>
+  )
 }
 
 export default App;
